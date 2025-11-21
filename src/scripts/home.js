76b19a94eca4dfd562196df8af490e4fb34747a7
rayx3000 <candole2020@gmail.com>
2025-11-21 10:56:20 +0800

    put menu content

---
+++ src/scripts/home.js
@@ -0,0 +1,16 @@
+import restaurant from '../assets/restaurant.png'
+
+const home = `
+    <div class="home-page">
+        <img src=${restaurant} alt="restaurant">
+        <h1>Welcome To Rayx Resto</h1>
+        <p>Rayx Resto is that wildly futuristic, neon-lit food temple 
+            where the whole world—and a few imaginary galaxies—collide 
+            on a plate, serving dishes crafted by an international roster of 
+            elite chefs who treat cuisine like high art. The ingredients 
+            are hyper-fresh, the tech is borderline sorcery, and the 
+            vibe is a multicultural fever dream where travelers, locals,
+                aliens, and food nerds share tables like it’s a spaceport 
+                café for the deliciously unhinged.</p>
+    </div>
+`
